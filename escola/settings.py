b848e4d9c5f81e63d15924dd62a9161d3aa7d61e
Elton Santos <elton.c.s1979@gmail.com>
2022-12-14 21:32:32 -0300

    Fix corrections

--- escola/settings.py
+++ escola/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qrv4!5acu=7m$9=_ojg@uyg#4uciy62v75*=*wh(&-xdw&f_ry'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
